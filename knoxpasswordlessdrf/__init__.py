68b52e51f617640c8823ed27b4606692fc66acc7
lijoev <lijo.varghese@seqato.com>
2019-10-04 20:27:45 +0530

    initial commit

---
+++ knoxpasswordlessdrf/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+__title__ = 'knoxpasswordlessdrf'
+__version__ = '1.3.7'
+__author__ = 'Lijo'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2019 lijo'
+
+# Version synonym
+VERSION = __version__
+
+default_app_config = 'knoxpasswordlessdrf.apps.DrfpasswordlessConfig'
---
+++ tests/urls.py
@@ -0,0 +1,17 @@
+from django.conf.urls import url
+from rest_framework.urlpatterns import format_suffix_patterns
+from knoxpasswordlessdrf.views import (ObtainEmailCallbackToken,
+                                   ObtainMobileCallbackToken,
+                                   ObtainAuthTokenFromCallbackToken,
+                                   VerifyAliasFromCallbackToken,
+                                   ObtainEmailVerificationCallbackToken,
+                                   ObtainMobileVerificationCallbackToken, )
+
+urlpatterns = [url(r'^callback/auth/$', ObtainAuthTokenFromCallbackToken.as_view(), name='auth_callback'),
+               url(r'^auth/email/$', ObtainEmailCallbackToken.as_view(), name='auth_email'),
+               url(r'^auth/mobile/$', ObtainMobileCallbackToken.as_view(), name='auth_mobile'),
+               url(r'^callback/verify/$', VerifyAliasFromCallbackToken.as_view(), name='verify_callback'),
+               url(r'^verify/email/$', ObtainEmailVerificationCallbackToken.as_view(), name='verify_email'),
+               url(r'^verify/mobile/$', ObtainMobileVerificationCallbackToken.as_view(), name='verify_mobile')]
+
+format_suffix_patterns(urlpatterns)
